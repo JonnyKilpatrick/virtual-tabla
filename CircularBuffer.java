61373e5c0b1f1e47b1ea71a8735d6803561e9275
Jonathan Kilpatrick <jpwkilpatrick1@sheffield.ac.uk>
2015-02-11 23:17:00 +0000

    First implementation of Karplus-strong algorithm using the Circular buffer instead of a normal delay line - working same as before (string sounds)

--- CircularBuffer.java
+++ CircularBuffer.java
@@ -1,11 +1,12 @@
 /**
  * Implementation for a Circular buffer.
- * Allows you to populate a circular buffer of a chosen type, then read / write from it, 
+ * Allows you to populate a circular buffer of doubles, then read / write from it, 
  * whilst internally keeping tracking of the read and write pointers. 
+ * Also allows a second read pointer
  * Specifically for the use in synthesis with the Karplus-Strong algorithm, so must specify desired delay length 
  */
  
-public class CircularBuffer<T>
+public class CircularBuffer
 {
   
   /**************************************************************************************************/
@@ -14,10 +15,11 @@ public class CircularBuffer<T>
   //
   /**************************************************************************************************/
 
-  private T[] circularBuffer;  // The actual array for the buffer of type T
-  private int readPointer;     // Read pointer
-  private int writePointer;    // Write pointer
-  private int bufferSize;      // Size of the buffer
+  private double[] circularBuffer; // The actual array for the buffer
+  private int readPointer;         // Read pointer
+  private int writePointer;        // Write pointer
+  private int bufferSize;          // Size of the buffer
+  private int secondReadPointer;   // Second read pointer
   
   
   /**************************************************************************************************/
@@ -29,15 +31,23 @@ public class CircularBuffer<T>
    * Class constructor
    * @param size int the size of the circular buffer
    * @param delayLength int the distance from the read to the write pointer
-   * @param initialData T[] the array of initial data to populate the circular buffer,
+   * @param delayOfSecondReadPointer int the distance from an optional second read pointer to the write pointer
+   * @param initialData double[] the array of initial data to populate the circular buffer,
    *   does not have to fill the entire circular buffer
    */
-  public CircularBuffer(int bufferSize, int delayLength, T[] initialData)
+  public CircularBuffer(int bufferSize, int delayLength, int delayOfSecondReadPointer, double[] initialData)
   {
-    this.bufferSize = bufferSize;
-    circularBuffer = (T[]) new Object[bufferSize];
+    // Check parameters 
+    if(bufferSize == 0)
+    {
+      throw new IllegalArgumentException("Cannot create a buffer of size 0!");
+    }
+    
+    this.bufferSize = bufferSize; //<>//
+    circularBuffer = new double[bufferSize];
     readPointer = 0;
     writePointer = delayLength % bufferSize;
+    secondReadPointer = Math.abs((writePointer - delayOfSecondReadPointer) % bufferSize); 
     
     // Populate circular buffer with initial data
     for(int i=0; i<initialData.length; i++)
@@ -54,17 +64,26 @@ public class CircularBuffer<T>
   /**************************************************************************************************/
   /**
    * Read the next value from the circular buffer, returning the value and moving the pointer on
+   * @return double[] the values of the first and second read pointer from the buffer
    */
    
-   public T read()
+   public double[] read()
    {
+     // FIRST READ POINTER
      // Get value at current pointer position
-     T value = circularBuffer[readPointer];
+     double pointer1 = circularBuffer[readPointer]; //<>//
      
      // Advance the read pointer
      readPointer = (readPointer + 1) % bufferSize;
      
-     return value;
+     // SECOND READ POINTER
+     // Get value at current pointer position
+     double pointer2 = circularBuffer[secondReadPointer];
+     
+     // Advance the second read pointer
+     secondReadPointer = (secondReadPointer + 1) % bufferSize;
+     
+     return new double[]{pointer1, pointer2};
    }
    
   /**************************************************************************************************/
@@ -74,9 +93,10 @@ public class CircularBuffer<T>
   /**************************************************************************************************/
   /**
    * Write the next value to the circular buffer, the moving on the write pointer
+   * @param value double the value to write to the buffer
    */
    
-   public void write(T value)
+   public void write(double value)
    {
      // Write value at current position
      circularBuffer[writePointer] = value;
@@ -85,22 +105,22 @@ public class CircularBuffer<T>
      writePointer = (writePointer + 1) % bufferSize;
    }
    
+   
+   
+   
    public void print()
    {
      String print = "";
-     for(T value: circularBuffer)
+     for(double value: circularBuffer)
      {
        print += value + ", ";
      }
      System.out.println(print);
    }
-
-
-   
    
    public static void main(String[] args)
    {
-     CircularBuffer<Integer> buffer = new CircularBuffer<Integer>(5, 5, new Integer[]{4, 2, 6, 4, 7});
+     CircularBuffer buffer = new CircularBuffer(5, 5, 5, new double[]{4f, 2f, 6f, 4f, 7f});
      System.out.println("Read: " + buffer.read());
      System.out.println("Write: 1");
      buffer.write(1);
--- DrumSynthNote.java
+++ DrumSynthNote.java
@@ -1,3 +1,266 @@
+//import processing.core.*;
+//import com.jsyn.*;
+//import com.jsyn.data.*;
+//import com.jsyn.unitgen.*;
+//import com.jsyn.util.*;
+//
+///**
+// * Class for a DrumSynthNote, synthesises tabla sounds given MidiMessages
+// */
+//
+//public class DrumSynthNote
+//{ 
+//  /**************************************************************************************************/
+//  //
+//  /* Instance variables 
+//  //
+//  /**************************************************************************************************/
+//  
+//  // JSyn Unit Generators
+//  
+//  Synthesizer synth;                           // JSyn synthesizer
+//  LineOut lineOut;                             // Output
+//  FixedRateMonoReader initialEvaluator;        // Mono Reader to output the initial values from the table into the delay line 
+//  FunctionEvaluator karplusStrongEvaluator;    // Evaluator to output the new value of the sample from the Karplus-Strong algorithm
+//  Delay delayLine;                             // DelayLine for the Karplus-Strong algorithm
+//  
+//  // Karplus-Strong parameters
+//  int samplingRate;
+//  double shorteningFactor;
+//  double stretchingFactor;
+//  double stretchingFactorTakeOne;
+//  
+//  
+//  /**************************************************************************************************/
+//  //
+//  /* Constructor 
+//  //
+//  /**************************************************************************************************/
+//  
+//  /**
+//   * Class constructor, sets up the JSyn unit generators to synthesise drum sounds
+//   */
+//  public DrumSynthNote(Synthesizer synth, LineOut lineOut)
+//  {
+//    
+//    // Setup
+//    try
+//    {  
+//      this.synth = synth;
+//      this.lineOut = lineOut;
+//      
+//      // Create initial mono reader to play the initial values in the wave table and feed them into the delay line
+//      initialEvaluator = new FixedRateMonoReader();
+//      synth.add(initialEvaluator);
+//
+//      // Create the actual delay line
+//      delayLine = new Delay();   
+//      // Allocate initial number of samples
+//      delayLine.allocate(1);
+//      synth.add(delayLine);
+//      
+//      // Create new function evaluator to evaluate the values from the table and update the table
+//      karplusStrongEvaluator = new FunctionEvaluator();
+//      synth.add(karplusStrongEvaluator);
+//       
+//      // Connect the initial mono reader to the line out to play initial samples
+//      // and to the input of the delay line to fill it with samples
+//      initialEvaluator.output.connect(0, lineOut.input, 0);
+//      initialEvaluator.output.connect(0, delayLine.input, 0);
+//      
+//      // Connect output of the delay line to the function evaluator to run the karplus strong algorithm
+//      delayLine.output.connect(0, karplusStrongEvaluator.input, 0);
+//       
+//      // Connect the output of the function evaluator to the line out
+//      // and back into the delay line to update the values from the table
+//      karplusStrongEvaluator.output.connect(0, lineOut.input, 0);
+//      karplusStrongEvaluator.output.connect(0, delayLine.input, 0);
+//      
+//      // Get the current default sampling rate, for Karplus-Strong calculations
+//      samplingRate = synth.getFrameRate();
+//    }
+//    // Handle errors
+//    catch (Exception ex)
+//    {
+//      ex.printStackTrace();
+//      //// ***** TODO throw new FileNotFoundException("Error: Unable to initialise drum synthesis");
+//    }
+//  }
+//  
+//  
+//  /**************************************************************************************************/
+//  //
+//  /* playNote 
+//  //
+//  /**************************************************************************************************/
+//  
+//  /**
+//   * Plays a single drum hit, given the paramaters to control the Karplus-Strong algorithm
+//   * @param fundimentalFrequency double the frequency of the note / hit
+//   * @param amplitude double the peak amplitude of the hit in dB
+//   * @param duration double the duration of the note / hit decay in seconds
+//   * @param q double the desired decay in dB
+//   */
+//   
+//   public void playNote(double fundimentalFrequency, double amplitude, double duration, double q)
+//   {
+//     lineOut.stop();
+//     // Work out p, the number of samples needed in the buffer for the chosen frequency
+//     double loop = samplingRate / fundimentalFrequency;
+//     int numSamples = (int) Math.round(loop);
+//     
+//     // Allocate a delay line of the right length
+//     delayLine.allocate(numSamples);
+//     
+//     // Set initial values for decay stretching / shortening and all pass filter
+//     shorteningFactor = 1.0;
+//     stretchingFactor = 0.5;
+//     double coefficient = 0.999;
+//     
+//     // Calculate G(f), the loop gain required at any frequency for the decay of q
+//     double gF = Math.pow(10.0, (-q/(20.0 * fundimentalFrequency * duration)));
+//     
+//     // Calculate G norm, the gain of the lowpass filter at the frequency
+//     double g = Math.cos((Math.PI * fundimentalFrequency) / samplingRate);
+//     
+//     // Compare gF and g
+//     
+//     // If gF <= g, decay shortening is needed
+//     if(gF <= g)
+//     {
+//       shorteningFactor = gF / g;
+//     }
+//     // Else decay stretching is needed
+//     else
+//     {
+//       // Solve quadratic formula
+//       
+//       double cosFundimental = Math.cos((2.0 * Math.PI * fundimentalFrequency) / samplingRate);
+//       double a = 2.0 - (2.0 * cosFundimental);
+//       double b = (2.0 * cosFundimental) - 2.0;
+//       double c = 1.0 - (gF * gF);
+//       double d = Math.sqrt((b * b) - (4.0 * a * c));
+//       double a2 = 2.0 * a;
+//       double stretching1 = (-b + d) / a2;
+//       double stretching2 = (-b - d) / a2;
+//       
+//       // Pick the value for stretchingFactor that stretches 
+//       if (stretching1 > 0 && stretching1 <= 0.5)
+//       {
+//         stretchingFactor = stretching1;
+//       }  
+//       else
+//       {
+//         stretchingFactor = stretching2;
+//       }
+//     }
+//     
+//     // Work out coefficient for allpass filter approach for fractional delay line
+//     double delay = numSamples + stretchingFactor;
+//     if(delay > loop)
+//     {
+//       delay = (numSamples - 1) + stretchingFactor;
+//     }
+//     // Store 1-S for forumla
+//     stretchingFactorTakeOne = 1 - stretchingFactor;
+//     
+//     double d = loop - delay;
+//     coefficient = (1 - d) / (1 + d);
+//     
+//     // Create delay line of the correct number of samples for chosen frequency, 
+//     // initialised with random numbers in the range -1 to 1.
+//     float[] initialWaveTable = generateRandomNumbers(numSamples, -1, 1);
+//     
+//     // Compute the average of the random numbers
+//     float average = computeAverage(initialWaveTable);
+//     
+//     // Take away the average from each number to ensure the values will have a 0 mean
+//     // This ensures the sound eventually dies away 
+//     // Also scale the random numbers for the chosen amplitude
+//     
+//     for (int i=0; i<numSamples; i++)
+//     {
+//       initialWaveTable[i] -= average;
+//       initialWaveTable[i] *= amplitude;
+//     }
+//     
+//     // Create function for the Karplus-Strong algorithm
+//     Function karplusStrong = new Function()
+//     {
+//       private double lastSample = 0;
+//       private double newSample = 0;
+//       
+//       public double evaluate(double sample)
+//       {
+//         newSample = shorteningFactor * ((stretchingFactorTakeOne * sample) + (stretchingFactor * lastSample));
+//         lastSample = sample;
+//         return newSample;
+//       }
+//     };
+//     // Add the function to the evaluator
+//     karplusStrongEvaluator.function.set(karplusStrong);
+//     
+//     // Load a float table into the function evaluator to play the initial samples
+//     FloatSample waveTable = new FloatSample(initialWaveTable);
+//     
+//     // Queue the initial values into the initial evaluator to fill the delay line
+//     initialEvaluator.dataQueue.clear();
+//     initialEvaluator.dataQueue.queue(waveTable, 0, numSamples);
+//
+//     // Start the processing to play sound
+//     lineOut.start();
+//   }
+//   
+//   /**
+//   * Compute the average of an array of floats
+//   * @param randomValues float[] the array of values
+//   * @retrun float the average of the array values
+//   */
+//   
+//   private float computeAverage(float[] randomValues)
+//   {  
+//     float total = 0;
+//     float arrayLength = (float) randomValues.length;
+//     
+//     for(int i=0; i<arrayLength; i++)
+//     {
+//       total += randomValues[i];  
+//     }
+//     
+//     return total / arrayLength;
+//   }
+//   
+//   
+//   /**
+//   * Return array of random float values between the given limits 
+//   * @param size int the number of random numbers to generate
+//   * @param min int the minimum boundary for the random numbers
+//   * @param max int the maxumum boundary for the random numbers
+//   * @retrun float[] the array of random numbers
+//   */
+//   
+//   private float[] generateRandomNumbers(int size, int min, int max)
+//   {
+//     int range = max - min;
+//     float[] randomNumbers = new float[size];
+//     
+//     for(int i=0; i<size; i++)
+//     {
+//       randomNumbers[i] = (float) (min + (range * Math.random()));
+//     }
+//     return randomNumbers;
+//   }
+//   
+//}
+
+
+
+
+
+
+
+
+
 import processing.core.*;
 import com.jsyn.*;
 import com.jsyn.data.*;
@@ -20,9 +283,8 @@ public class DrumSynthNote
   
   Synthesizer synth;                           // JSyn synthesizer
   LineOut lineOut;                             // Output
-  FixedRateMonoReader initialEvaluator;        // Mono Reader to output the initial values from the table into the delay line 
   FunctionEvaluator karplusStrongEvaluator;    // Evaluator to output the new value of the sample from the Karplus-Strong algorithm
-  Delay delayLine;                             // DelayLine for the Karplus-Strong algorithm
+  CircularBuffer buffer;                       // Circular buffer to store the values
   
   // Karplus-Strong parameters
   int samplingRate;
@@ -49,32 +311,57 @@ public class DrumSynthNote
       this.synth = synth;
       this.lineOut = lineOut;
       
-      // Create initial mono reader to play the initial values in the wave table and feed them into the delay line
-      initialEvaluator = new FixedRateMonoReader();
-      synth.add(initialEvaluator);
-
-      // Create the actual delay line
-      delayLine = new Delay();   
-      // Allocate initial number of samples
-      delayLine.allocate(1);
-      synth.add(delayLine);
-      
-      // Create new function evaluator to evaluate the values from the table and update the table
+      // Create new function evaluator to evaluate the values from the buffer and update it
       karplusStrongEvaluator = new FunctionEvaluator();
       synth.add(karplusStrongEvaluator);
-       
-      // Connect the initial mono reader to the line out to play initial samples
-      // and to the input of the delay line to fill it with samples
-      initialEvaluator.output.connect(0, lineOut.input, 0);
-      initialEvaluator.output.connect(0, delayLine.input, 0);
       
-      // Connect output of the delay line to the function evaluator to run the karplus strong algorithm
-      delayLine.output.connect(0, karplusStrongEvaluator.input, 0);
+      // Initialise circular buffer with empty fields
+      buffer = new CircularBuffer(1, 1, 1, new double[1]);
+
+      // Create function for the Karplus-Strong algorithm
+      Function karplusStrong = new Function()
+      {
+        private double lastSample = 0;
+        private double newSample = 0;
+        private double sample = 0;
+        
+        private double lastSample2 = 0;
+        private double newSample2 = 0;
+        private double sample2 = 0;
+        
+        private double blendFactor = 1;
+       
+        public double evaluate(double x)
+        {
+          double[] samples = buffer.read();
+          
+          sample = samples[0];
+         
+          newSample = shorteningFactor * ((stretchingFactorTakeOne * sample) + (stretchingFactor * lastSample));
+          lastSample = sample;
+          
+          sample2 = samples[1];
+         
+          newSample2 = shorteningFactor * ((stretchingFactorTakeOne * sample2) + (stretchingFactor * lastSample2));
+          lastSample2 = sample2;
+          
+          double result = (blendFactor * newSample) + ((1-blendFactor) * newSample2);
+          
+          if(blendFactor>=0)
+          {
+            blendFactor -= 0.000001;
+          }
+         
+          buffer.write(result);
+         
+          return newSample;
+        }
+      };
+      // Add the function to the evaluator
+      karplusStrongEvaluator.function.set(karplusStrong);
        
       // Connect the output of the function evaluator to the line out
-      // and back into the delay line to update the values from the table
       karplusStrongEvaluator.output.connect(0, lineOut.input, 0);
-      karplusStrongEvaluator.output.connect(0, delayLine.input, 0);
       
       // Get the current default sampling rate, for Karplus-Strong calculations
       samplingRate = synth.getFrameRate();
@@ -105,12 +392,10 @@ public class DrumSynthNote
    public void playNote(double fundimentalFrequency, double amplitude, double duration, double q)
    {
      lineOut.stop();
+     
      // Work out p, the number of samples needed in the buffer for the chosen frequency
      double loop = samplingRate / fundimentalFrequency;
      int numSamples = (int) Math.round(loop);
-     
-     // Allocate a delay line of the right length
-     delayLine.allocate(numSamples);
      
      // Set initial values for decay stretching / shortening and all pass filter
      shorteningFactor = 1.0;
@@ -169,10 +454,10 @@ public class DrumSynthNote
      
      // Create delay line of the correct number of samples for chosen frequency, 
      // initialised with random numbers in the range -1 to 1.
-     float[] initialWaveTable = generateRandomNumbers(numSamples, -1, 1);
+     double[] initialWaveTable = generateRandomNumbers(numSamples, -1, 1);
      
      // Compute the average of the random numbers
-     float average = computeAverage(initialWaveTable);
+     double average = computeAverage(initialWaveTable);
      
      // Take away the average from each number to ensure the values will have a 0 mean
      // This ensures the sound eventually dies away 
@@ -184,28 +469,8 @@ public class DrumSynthNote
        initialWaveTable[i] *= amplitude;
      }
      
-     // Create function for the Karplus-Strong algorithm
-     Function karplusStrong = new Function()
-     {
-       private double lastSample = 0;
-       private double newSample = 0;
-       
-       public double evaluate(double sample)
-       {
-         newSample = shorteningFactor * ((stretchingFactorTakeOne * sample) + (stretchingFactor * lastSample));
-         lastSample = sample;
-         return newSample;
-       }
-     };
-     // Add the function to the evaluator
-     karplusStrongEvaluator.function.set(karplusStrong);
-     
-     // Load a float table into the function evaluator to play the initial samples
-     FloatSample waveTable = new FloatSample(initialWaveTable);
-     
-     // Queue the initial values into the initial evaluator to fill the delay line
-     initialEvaluator.dataQueue.clear();
-     initialEvaluator.dataQueue.queue(waveTable, 0, numSamples);
+     // Create the circular buffer and give it the initial data samples
+     buffer = new CircularBuffer(numSamples, numSamples-15, numSamples-25, initialWaveTable);
 
      // Start the processing to play sound
      lineOut.start();
@@ -213,14 +478,14 @@ public class DrumSynthNote
    
    /**
    * Compute the average of an array of floats
-   * @param randomValues float[] the array of values
-   * @retrun float the average of the array values
+   * @param randomValues double[] the array of values
+   * @retrun double the average of the array values
    */
    
-   private float computeAverage(float[] randomValues)
+   private double computeAverage(double[] randomValues)
    {  
-     float total = 0;
-     float arrayLength = (float) randomValues.length;
+     double total = 0;
+     double arrayLength = (double) randomValues.length;
      
      for(int i=0; i<arrayLength; i++)
      {
@@ -232,21 +497,21 @@ public class DrumSynthNote
    
    
    /**
-   * Return array of random float values between the given limits 
+   * Return array of random double values between the given limits 
    * @param size int the number of random numbers to generate
    * @param min int the minimum boundary for the random numbers
    * @param max int the maxumum boundary for the random numbers
-   * @retrun float[] the array of random numbers
+   * @retrun double[] the array of random numbers
    */
    
-   private float[] generateRandomNumbers(int size, int min, int max)
+   private double[] generateRandomNumbers(int size, int min, int max)
    {
      int range = max - min;
-     float[] randomNumbers = new float[size];
+     double[] randomNumbers = new double[size];
      
      for(int i=0; i<size; i++)
      {
-       randomNumbers[i] = (float) (min + (range * Math.random()));
+       randomNumbers[i] = min + (range * Math.random());
      }
      return randomNumbers;
    }
